6ef17808e000f422e7d43edbd6690d437f27cb9c
wang_shi_hua <wang_shi_hua@126.com>
2020-01-09 17:30:01 +0800

    noeureka

--- springcloud-eureka-seata/account/src/main/java/io/seata/sample/AccountApplication.java
+++ springcloud-eureka-seata/account/src/main/java/io/seata/sample/AccountApplication.java
@@ -1,26 +1,24 @@
 package io.seata.sample;
 
-import javax.sql.DataSource;
-
 import com.alibaba.druid.pool.DruidDataSource;
-
 import io.seata.rm.datasource.DataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+
 /**
  * @author jimin.jm@alibaba-inc.com
  * @date 2019/06/14
  */
-@SpringBootApplication
-@EnableEurekaClient
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 @EnableFeignClients
 public class AccountApplication {
 
--- springcloud-eureka-seata/order/src/main/java/io/seata/sample/OrderApplication.java
+++ springcloud-eureka-seata/order/src/main/java/io/seata/sample/OrderApplication.java
@@ -1,19 +1,17 @@
 package io.seata.sample;
 
-import javax.sql.DataSource;
-
 import com.alibaba.druid.pool.DruidDataSource;
-
 import io.seata.rm.datasource.DataSourceProxy;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
 
 /**
  * @author jimin.jm@alibaba-inc.com
@@ -21,7 +19,6 @@ import org.springframework.jdbc.core.JdbcTemplate;
  */
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
 public class OrderApplication {
 
     public static void main(String[] args) {
